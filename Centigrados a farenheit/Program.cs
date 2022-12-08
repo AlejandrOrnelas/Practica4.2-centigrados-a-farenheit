a6a101ef85b0418aa11067e85ab8dc64efb0d031
alejandro ornelas montoya <hussong1234567@live.com>
2022-12-07 21:15:38 -0800

    Agregar archivos de proyecto.

---
+++ Centigrados a farenheit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCentigrados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double c, f;
+
+            Console.WriteLine("Dame os datos de los centigrados");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            f = (c * 1.8) + 32;
+
+            Console.WriteLine("Los grados convertidos a Farenheit son:{0}", f);
+
+        }
+    }
+}
